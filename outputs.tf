0c3adca1da8419410bdf02835b3563fb34c9a6d7
Dmytro Morozov <mdmsua@icloud.com>
2024-03-02 14:09:17 +0100

    Update outputs descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "name" {
-  value = azurerm_virtual_wan.main.name
+  value       = azurerm_virtual_wan.main.name
+  description = "Virtual WAN name"
 }
 
 output "resource_group_name" {
-  value = azurerm_virtual_wan.main.resource_group_name
+  value       = azurerm_virtual_wan.main.resource_group_name
+  description = "Virtual WAN resource group name"
 }
 
 output "subscription_id" {
-  value = data.azurerm_subscription.main.subscription_id
+  value       = data.azurerm_subscription.main.subscription_id
+  description = "Virtual WAN subscription ID"
 }
 
 output "private_dns_zones" {
-  value = [azurerm_private_dns_zone.main[*].name]
+  value       = [azurerm_private_dns_zone.main[*].name]
+  description = "List of private DNS zones"
 }
